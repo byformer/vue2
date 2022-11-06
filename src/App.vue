0d2f63b211aeee102af130797adaa91c469f3b5f
former <1633852615@qq.com>
2022-11-06 14:46:05 +0800

    vue2-demo

---
+++ src/App.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="continuer">
+    <!-- 头部 -->
+    <header>
+      <div class="header">
+        <el-row type="flex" class="row-bg" justify="space-between">
+          <el-col :span="6"
+            ><div class="grid-content bg-purple bg-left bg-header">
+              <h1 class="h1">Hello,World!</h1>
+            </div></el-col
+          >
+
+          <el-col :span="12"
+            ><div class="grid-content bg-purple bg-right bg-header">
+              <li>
+                <a href="javascript:;">{{ home }}</a>
+              </li>
+              <li>
+                <a href="javascript:;">{{ blog }}</a>
+              </li>
+              <li>
+                <a href="javascript:;">{{ github }}</a>
+              </li>
+              <li>
+                <a href="javascript:;">{{ about }}</a>
+              </li>
+              <li>
+                <a href="javascript:;">{{ resume }}</a>
+              </li>
+            </div></el-col
+          >
+        </el-row>
+      </div>
+    </header>
+    <!-- 内容 -->
+    <section>
+      <el-row type="flex" class="row-bg bg-set" justify="space-around">
+        <el-col :span="7" class="set"
+          ><div class="grid-content bg-purple set-left">
+            <p>
+              {{ h1 }}
+              <br />
+              {{ h2 }}
+              <br />
+              {{ h3 }}
+              <br />
+              {{ h4 }}
+              <br />
+              <br />
+              {{ h5 }}
+              <br />
+              {{ h6 }}
+              <br />
+              {{ h7 }}
+              <br />
+            </p></div
+        ></el-col>
+
+        <el-col :span="13"
+          ><div class="grid-content bg-purple set-right">
+            <page></page></div
+        ></el-col>
+      </el-row>
+    </section>
+    <!-- 底部 -->
+
+    <buyCat></buyCat>
+
+    <footer>
+      <p> {{by}}</p>
+     <a href="mailto:1633852615@qq.com" target="_blanket">
+      <img src="./assets/pic6.svg" alt="">
+      <span>{{email}}</span>
+     </a>
+    </footer>
+  </div>
+</template>
+
+<script>
+import page from "./components/page.vue";
+import buyCat from "./components/buyCat.vue";
+
+// import typing from './components/typing.vue';
+export default {
+  components: {
+    page,
+    buyCat,
+  },
+  data() {
+    return {
+      h1: "谎言的代价是什么？",
+      h2: "并非是我们会把谎言误认为真相。",
+      h3: "真正危险的是，",
+      h4: "我们听多了谎言，便不再能分辨出真相。",
+      h5: "那时我们能做什么？",
+      h6: "除了放弃对真理的信仰，满足于谎言堆砌的故事。",
+      h7: "我们还剩下什么？",
+      home: "Home",
+      blog: "Blog",
+      github: "GitHub",
+      about: "About",
+      resume: "Resume",
+      by:'Powered by PProfile',
+      email:'1633852615@qq.com'
+    };
+  },
+  modules: {},
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: #000;
+}
+header {
+  position: relative;
+  height: 80px;
+  background-color: #fff;
+  z-index: 20;
+}
+.header {
+  position: fixed;
+  background-color: #fff;
+  width: 100vw;
+  height: 80px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+}
+.bg-header {
+  height: 80px;
+}
+.bg-left {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bg-purple .h1 {
+  color: #161b3d;
+  font-size: 35px;
+}
+.bg-right {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+/* section */
+section {
+  height: 523px;
+  background-color: #f8f8f8;
+}
+.bg-set {
+  height: 500px;
+  padding-top: 50px;
+}
+
+.set-left {
+  position: relative;
+}
+.set-left p {
+  position: absolute;
+  top: 25px;
+  left: 25px;
+  font-size: 20px;
+  line-height: 35px;
+}
+/* footer */
+
+footer {
+  height: 50px;
+  padding: 1rem 2rem;
+  display:flex;
+  justify-content:space-between;
+}
+footer p{
+  color:#919296;
+}
+footer a{
+  display: flex;
+}
+footer a img{
+  width: 20px;height:20px;
+  vertical-align: middle;
+}
+footer a span{
+  color:#919296;
+  margin-left:10px;
+}
+</style>
---
+++ src/components/buyCat.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="chat">
+    <ul>
+      <li class="active">
+        <img src="../assets/pic1.svg" />
+        <span>{{ all }}</span>
+      </li>
+      <li>
+        <img src="../assets/pic2.svg" />
+        <span>{{ vue }}</span>
+      </li>
+
+      <li>
+        <img src="../assets/pic4.svg" />
+        <span>{{ react }}</span>
+      </li>
+      <li>
+        <img src="../assets/pic5.svg" />
+        <span>{{ js }}</span>
+      </li>
+    </ul>
+    <!-- 内容 -->
+    <div class="content">
+      <el-row type="flex" class="row-bg" justify="space-around">
+        <el-col :span="7">
+          <div class="grid-content bg-purple project">
+            <span class="icon" style="background-color: rgb(69, 148, 8)"
+              >V</span
+            >
+            <a href="javascript:;" target="_blanket">
+              <strong>Vue-demo</strong>
+              <p>
+                这是一个Vue2.x的树组件。它允许你去拖拽任意节点，当然，“交换”会反映在data数据中。
+              </p>
+            </a>
+          </div>
+        </el-col>
+        <el-col :span="7"
+          ><div class="grid-content bg-purple-light project">
+            <span class="icon" style="background-color: rgb(232, 239, 24)"
+              >JS</span
+            >
+            <a href="javascript:;" target="_blanket">
+              <strong>JS-demo</strong>
+              <p>
+                ----------------------------暂无内容----------------------------------
+              </p>
+            </a>
+          </div>
+        </el-col>
+        <el-col :span="7"
+          ><div class="grid-content bg-purple project">
+            <span class="icon" style="background-color: rgb(20, 111, 246)"
+              >R</span
+            >
+            <a href="javascript:;" target="_blanket">
+              <strong>React-demo</strong>
+              <p>
+                ----------------------------暂无内容----------------------------------
+              </p>
+            </a>
+          </div></el-col
+        >
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounters: {},
+  data() {
+    return {
+      all: "All Projects",
+      vue: "Vue",
+      js: "JavaScript",
+      react: "React",
+    };
+  },
+  mounted() {},
+};
+</script>
+
+<style>
+.chat {
+  height: 500px;
+  border-bottom: 1px solid #ccc;
+}
+.chat ul {
+  display: flex;
+  height: 50px;
+  border-bottom: 1px solid rgb(231, 230, 230);
+  margin-bottom: 50px;
+  /* box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.1) */
+  /* background-color: rgb(20, 111, 246) */
+}
+.chat ul li {
+  margin: 0 15px;
+  /* text-align: center; */
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.chat ul li img {
+  width: 20px;
+  height: 20px;
+  vertical-align: middle;
+}
+.chat ul li span {
+  text-align: center;
+  padding-left: 10px;
+}
+/* 内容 */
+.project {
+  display: flex;
+  align-items: center;
+}
+.project span {
+  width: 80px;
+  height: 40px;
+  display: inline-block;
+  margin-right: 10px;
+  text-align: center;
+  line-height: 40px;
+  border-radius: 0.25rem;
+  font-size: 1.25rem;
+  color: #fff;
+  font-weight: 700;
+}
+.project a strong {
+  margin: 0 20px 0 10px;
+}
+.project a p {
+  display: inline;
+  font-weight: normal;
+  width: 100px;
+}
+.active {
+  border-bottom: 3px solid #e13f5e;
+}
+</style>
